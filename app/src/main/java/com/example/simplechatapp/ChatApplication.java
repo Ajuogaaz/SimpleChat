bba106eb564eb4a770b012e62fd1def27659ea89
Linus Brian Okoth <okoth@fb.com>
2020-07-06 12:53:36 -0400

    Added dependencies

--- app/src/main/java/com/example/simplechatapp/ChatApplication.java
+++ app/src/main/java/com/example/simplechatapp/ChatApplication.java
@@ -2,7 +2,35 @@ package com.example.simplechatapp;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
 public class ChatApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Use for monitering parse network traffick
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+
+        //Can be leve.Basic Level.Headers or Level Body
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        //set application Id and server based on the values on the Heroku settings
+        //anyNetwork interceptors must be added with the configuration builder given in this syntaxt
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+            .applicationId("simplechat-client")
+        .clientBuilder(builder)
+        .server("https://codepath-chat-lab.herokuapp.com/parse/").build());
 
 
+
+
+    }
 }
