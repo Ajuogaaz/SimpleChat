7c47ba19437ba0bdf9a8436f949a2dd20123522c
Linus Brian Okoth <okoth@fb.com>
2020-07-06 13:03:20 -0400

    Added login co mpenent

--- app/src/main/java/com/example/simplechatapp/ChatActivity.java
+++ app/src/main/java/com/example/simplechatapp/ChatActivity.java
@@ -3,6 +3,12 @@ package com.example.simplechatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -11,6 +17,29 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chat);
+
+        //User login
+        if (ParseUser.getCurrentUser() != null){
+            startWithCurrentUser();
+
+        }else{
+            login();
+        }
+    }
+
+    private void login() {
+        ParseAnonymousUtils.logIn(new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Anonympous Login Failed");
+                }
+            }
+        });
+
+    }
+
+    private void startWithCurrentUser() {
     }
 }
